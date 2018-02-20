0f9c5c772ae63f5ec9839fe539d9f34354aaa060
Daniel Agar <daniel@agar.ca>
2018-02-19 20:11:05 -0500

    move all configs to boards/

---
+++ boards/atlflight/eagle/default.cmake
@@ -0,0 +1,22 @@
+# The Eagle board is the first generation Snapdragon Flight board by Qualcomm.
+
+px4_add_board(
+	BOARD posix_eagle_default
+	OS posix
+	)
+
+# This cmake config builds for POSIX, so the part of the flight stack running
+# on the Linux side of the Snapdragon.
+include(../sdflight/default)
+
+set(CMAKE_TOOLCHAIN_FILE ${PX4_SOURCE_DIR}/cmake/cmake_hexagon/toolchain/Toolchain-arm-linux-gnueabihf.cmake)
+
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${PX4_SOURCE_DIR}/cmake/cmake_hexagon")
+
+set(CONFIG_SHMEM "1")
+
+# This definition allows to differentiate if this just the usual POSIX build
+# or if it is for the Snapdragon.
+add_definitions(
+	-D__PX4_POSIX_EAGLE
+)
